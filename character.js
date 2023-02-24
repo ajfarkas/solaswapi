4388caafa8cc353656509d8673be053b0d2039db
AJ Farkas <farkas.aj@gmail.com>
2023-02-24 11:23:19 -0500

    fill home world and style character page

--- character.js
+++ character.js
@@ -1,10 +1,34 @@
-const characterEl = document.getElementById('character');
+const $id = id => document.getElementById(id);
+const characterEl = $id('character');
 
 // Get character id from location.search string
 const defaultCharacter = location.search.replace('?', '').split(';').reduce((_, param) => {
 	const [key, val] = param.split('=');
 	if (key === 'id' || key === 'name') return val;
 }, null);
+
+renderAttributes = data => {
+	[
+		'name',
+		'birth_year',
+		'height',
+		'mass',
+		'hair_color',
+		'eye_color',
+		'skin_color'
+	].forEach(attr => {
+		$id(attr).innerText = data[attr];
+	});
+	fetch(data.homeworld, {
+		method: 'GET',
+		cache: 'force-cache',
+	}).then(res => {
+		if (res.status >= 400) throw new Error(res.status);
+		return res.json();
+	}).then(homeData => {
+		$id('homeworld').innerText = homeData.name;
+	})
+}
 
 // fetch the selected character, and populate the page
 const fetchCharacter = (charVal) => {
@@ -23,7 +47,7 @@ const fetchCharacter = (charVal) => {
 			const charInfo = Array.isArray(data)
 				? data.results[0]
 				: data;
-			characterEl.innerText = JSON.stringify(charInfo);
+			renderAttributes(charInfo);
 		})
 		.catch(console.error);
 };
